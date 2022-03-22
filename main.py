f853a1c8d290937a4db40b9f27e2fce75a0bb6f1
JaeyoungAhn <quaki@naver.com>
2022-03-23 01:19:19 +0900

    2022-03-22

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy
+import matplotlib.dates as mdates
+import datetime
+from scipy import stats
 
-data = pd.read_csv("data.csv", low_memory=False)
-#data.drop([],[],[],[],[],[], axis=1, inplace=True)
-print(data)
-print(data.shape)
-print(data.isna().sum())
+data = pd.read_csv("WHO-COVID-19-global-data.csv", low_memory=False)
+
+dates = data.loc[data['Country_code']=='KR'][['Date_reported']]
+dates = dates.values.tolist()
+dates = [datetime.datetime.strptime(str(d),"['%Y-%m-%d']").date() for d in dates]
+ax = plt.gca()
+formatter = mdates.DateFormatter("%Y-%m-%d")
+locator = mdates.DayLocator()
+
+fig = plt.figure()
+fig.suptitle('No. of New COVID-19 Cases Daily in Korea', fontsize=20)
+plt.xticks(rotation=20)
+
+newCase = data.loc[data['Country_code']=='KR'][['New_cases']]
+
+x = np.arange(46)+1
+targ=newCase[760:806].values.tolist()
+targ=sum(targ,[])
+
+slope, intercept, r, p, std_err = stats.linregress(x,targ)
+
+def output(x):
+    return slope * x + intercept
+
+mymodel = list(map(output, x))
+
+print(mymodel)
+plt.plot(dates[760:806],mymodel)
+plt.scatter(dates[760:806],newCase[760:806])
+plt.show()
+print(output(53))
+
+
+
+print(slope)
+print(intercept)
+print(r)
